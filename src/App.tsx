a9de2edc8a7d809ece79366ab028185df05f47e1
Inigojeevan <in1442@srmist.edu.in>
2024-03-27 22:39:08 +0530

    feat: bg-change

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import {BackgroundBeams} from "../components/ui/background-beams"
- // import './index.css'
-export default function App() {
+import styled from 'styled-components';
+
+const App = () => {
   return (
-    <main>
-      <BackgroundBeams />
-    </main>
-  );
+   <>
+      <Title>Focus<span className="green">Flora</span></Title>
+
+   </> 
+  )
 }
+
+export default App;
+
+const Title = styled.h1`
+  font-size: 6rem;
+  padding: 2rem 0;
+  text-align: center;
+
+  .green {
+    color: lightgreen
+  }
+  `;
